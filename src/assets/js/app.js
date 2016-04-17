3470b36fc45067cc3ab9daf3f9571a4470ad4ea2
Leonardo Melo <leomeloxp@gmail.com>
2016-04-17 19:38:20 +0100

    Adiciona integração com API e melhora o design
    Este commit adiciona a possibilidade de interagir com a API do InternetSemLimites.
    Por questões de simplicidade e praticidade, toda a integração é feita utilizando jQuery.
    
    Quando um usuário carrega a página, duas chamadas são feitas para a API,
    para que possamos preencher os campos do *Hall of Shame* e o mapa interativo com provedores de internet ilimitada.
    
    A partir daí, para cada vez que o usuário clicar em um dos estados que possuem provedores, uma nova chamada é feita e uma lista de provedores que servem aquele estados é exibida na caixa ao lado do mapa.
    
    O botão de adicionar um novo provedor também funciona, direcionando o usuário para o formulário da API para que ele possa adicionar um provedor a API.
    
    Esse commit também remove a área em que sugestões de como ajudar seriam exibidas. Isto é devido ao fato de eu não ter conteúdo para dada secção.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,165 @@
 $(document).foundation();
+
+// Little helper function to remove duplicate values of an array
+function getUnique(arr) {
+   var u = {}, a = [];
+   for(var i = 0, l = arr.length; i < l; ++i){
+      if(u.hasOwnProperty(arr[i])) {
+         continue;
+      }
+      a.push(arr[i]);
+      u[arr[i]] = 1;
+   }
+   return a;
+}
+
+// Anything related to the API that can/will be reused should be placed in there
+var API = {
+    // Default API url
+    'url': 'https://internetsemlimites.herokuapp.com/api/',
+    // Maps acronym to State names for pretty printing
+    'statesMap': {
+        'AC': 'Acre',
+        'AL': 'Alagoas',
+        'AM': 'Amazonas',
+        'AP': 'Amapá',
+        'BA': 'Bahia',
+        'CE': 'Ceará',
+        'DF': 'Distrito Federal',
+        'ES': 'Espírito Santo',
+        'GO': 'Goiás',
+        'MA': 'Maranhão',
+        'MG': 'Minas Gerais',
+        'MS': 'Mato Grosso do Sul',
+        'MT': 'Mato Grosso',
+        'PA': 'Pará',
+        'PB': 'Paraíba',
+        'PE': 'Pernambuco',
+        'PI': 'Piauí',
+        'PR': 'Paraná',
+        'RJ': 'Rio de Janeiro',
+        'RN': 'Rio Grande do Norte',
+        'RO': 'Rondônia',
+        'RR': 'Roraima',
+        'RS': 'Rio Grande do Sul',
+        'SC': 'Santa Catarina',
+        'SE': 'Sergipe',
+        'SP': 'São Paulo',
+        'TO': 'Tocantins'
+    }
+};
+
+
+
+$(document).ready(function($) {
+    // Populates the interactive map with states that have Unlimited data providers
+    $.ajax({
+        url: API.url + 'fame/',
+        type: 'GET',
+        dataType: 'json',
+    })
+    .done(function(data) {
+
+        var providers = data.providers;
+        var estados = [];
+
+        // Get the states that each provider covers and add it to an array
+        for(var i = 0; i < providers.length; i++) {
+            var provider = providers[i];
+            estados = estados.concat(provider.coverage);
+        }
+
+        // Remove duplicate entries from the array and add the hasProvider class to the related SVG markup in the page
+        estados = getUnique(estados);
+        for (var i = 0; i <  estados.length; i++) {
+            $('#' + estados[i]).addClass('hasProvider');
+        }
+    })
+    .fail(function(err) {
+        console.log('error: ', err);
+    });
+
+    // Populates the hall of shame on page load.
+    $.ajax({
+        url: API.url + 'shame/',
+        type: 'GET',
+        dataType: 'json',
+    })
+    .done(function(data) {
+
+        // Creates an array with jQuery objects that will be appended to the markup down below
+        var providers = data.providers;
+        var list = [];
+        for(var i = 0; i < data.providers.length; i++) {
+            list.push(
+                $('<div />', {'class': 'column text-center'}).append(
+                    $('<h3 />').text(providers[i].name),
+                    $('<p />').append(
+                        $('<a />', {'href': providers[i].source}).text('Fonte')
+                    )
+                )
+            );
+        }
+        $('.isp--hell').append(list);
+    })
+    .fail(function(err) {
+        console.log("error: ", err);
+    });
+
+});
+
+/**
+ * On click of every state in the interactive map we will make an API call to fetch all the providers
+ * that offer unlimited internet and covers that state. We'll then use that information to populate
+ * the list next to the map with that information.
+ */
+$(document).on('click', '.estados.hasProvider', function(event) {
+    // Return early since this is already the currently selected state
+    if ($(this).hasClass('active')) {
+        return;
+    }
+
+    var id = $(this).attr('id');
+    $.ajax({
+        url: API.url + id + '/fame/',
+        type: 'GET',
+        dataType: 'json',
+    })
+    .done(function(data) {
+
+        // If for some reason the call succeds but we don't get a valid list of providers, return early.
+        if (id && providers.length > 0) {
+            $('.isp-list--content').html('');
+        } else {
+            return;
+        }
+
+        // Call was successful, let's clear up the old selected state in the map
+        $('.estados').removeClass('active');
+
+        // Update the title of the card to match the currently selected state.
+        $('.state-card--title').text(API.statesMap[id]);
+
+        // Loop through each of the returned providers to create a list of jQuery objects that will
+        // be appended to the markup
+        var list = [];
+        for(var i = 0; i < providers.length; i++) {
+            list.push(
+                $('<div />', {'class': 'isp-list--isp'}).append(
+                    $('<span />', {'class': 'isp-list--left'}).text(providers[i].name),
+                    $('<span />', {'class': 'isp-list--right'}).append(
+                        $('<a />', {'href': providers[i].source}).text('Link')
+                    )
+                )
+            );
+        }
+        $('.isp-list--content').append(list);
+
+        // Add the .active class to the selected state since we'll now be displaying its information
+        $('#' + id).addClass('active');
+    })
+    .fail(function(err) {
+        console.log('error: ',err);
+    })
+
+});
